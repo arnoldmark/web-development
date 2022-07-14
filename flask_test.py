415e47b240877fc1e9b52285708b8109fb6383a5
Mark Karomo <mk772@cairn.edu>
2022-07-14 01:53:50 +0000

    main py file

---
+++ flask_test.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = '84afbd7d31ef4b66d6b583d022b3e6bc'    
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+   
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit() 
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) 
+    return render_template('register.html', title='Register', form=form)
+
+
+#You need to add a route for home you can use (/) then point it to the home.html page
+
+@app.route("/")                        
+def home():
+    return render_template('home.html', title='Home')
+
+
+if __name__ == '__main__':               
+    app.run(debug=True, host="0.0.0.0")
